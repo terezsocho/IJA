ccef0200dd6250a038a06b9ec390d1830fbd9839
AdPia123 <xpiace00@stud.fit.vutbr.cz>
2020-05-17 11:01:36 +0200

    Comments added

--- version.1.0/IJA-projekt/src/Sources/TimeChange.java
+++ version.1.0/IJA-projekt/src/Sources/TimeChange.java
@@ -1,9 +1,13 @@
+/*
+ * Authors: Terézia Sochova(xsocho14), Adrián Piaček(xpiace00)
+ * Source code TimeChange.java contains methods used for determination of Time change and its subsequent methods used for
+ * specific assignment functionality.
+ */
 package Sources;
 import java.util.Objects;
 
 public class TimeChange {
         private boolean time_changed;
-
         public TimeChange(boolean time_changed){
             this.time_changed = time_changed;
         }
@@ -17,5 +21,6 @@ public class TimeChange {
     public void setFalse() {
         this.time_changed = false;
     }
-    }
+
+}
 
